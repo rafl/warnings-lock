4fdb66cebf0c372c95b3a876c343f69ff274d62e
Florian Ragwitz <rafl@debian.org>
2015-07-07 13:19:57 -0700

    Add a SYNOPSIS

--- lib/warnings/lock.pm
+++ lib/warnings/lock.pm
@@ -2,11 +2,40 @@ use strict;
 use warnings;
 
 package warnings::lock;
-# ABSTRACT: lock warnings
+# ABSTRACT: lock down the set of warnings used in a lexical scope
 
 our $VERSION = '1';
 
 use Variable::Magic 'wizard', 'cast';
+
+=head1 SYNOPSIS
+
+    # Set up warnings just the way we want them
+    use warnings;
+    use feature 'postderef';
+    no warnings 'experimental::postderef';
+
+    # Lock warnings into their current state
+    use warnings::lock;
+
+    # Import additional modules which try to fiddle with our warning bits
+    use Moose;
+
+    # Use of postderef feature will not warn
+    []->@*
+
+    {
+        no warnings::lock;
+        no warnings 'recursion';
+
+        # deep recursion warnings disabled for the rest of this block
+        ...
+    }
+
+    # deep recursion warnings enabled again after the block
+    ...
+
+=cut
 
 my $hints_key = __PACKAGE__ . '/desired_warning_bits';
 
