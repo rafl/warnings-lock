635fd08264db976398fa2b3898a817b538132ea8
Florian Ragwitz <rafl@debian.org>
2015-07-08 10:26:15 -0700

    Improve Pod formatting

--- lib/warnings/lock.pm
+++ lib/warnings/lock.pm
@@ -26,31 +26,38 @@ use Variable::Magic 'wizard', 'cast';
 
 =head1 DESCRIPTION
 
-    Lock down the set of warnings used in a lexical scope
+Lock down the set of warnings used in a lexical scope
 
 =head1 MOTIVATION
 
 =head2 Background
 
-    Generally, we want warnings enabled. So, it seems like a good idea to C<use warnings>.
+Generally, we want warnings enabled. So, it seems like a good idea to C<use
+warnings>.
 
-    However, in some cases, we want to turn particular warnings off. So, we might say something like: C<no warnings 'experimental::signatures'>.
+However, in some cases, we want to turn particular warnings off. So, we might
+say something like: C<no warnings 'experimental::signatures'>.
 
-    But, if we later use a module like Foo which not only uses warnings, but (re) imports them, they'll be turned back on.
+But, if we later use a module like Foo which not only uses warnings, but (re)
+imports them, they'll be turned back on.
 
 =head2 Solution + Problem
 
-    We could just switch the order and use Foo before we turn off a particular warning.
+We could just switch the order and use Foo before we turn off a particular warning.
 
-    But what if we need the 'no warnings' to come first? Maybe the 'no warnings' is part of site-wide pragma which loads various safety measures?
+But what if we need the 'no warnings' to come first? Maybe the 'no warnings' is
+part of site-wide pragma which loads various safety measures?
 
-    We could write a second site-wide pragma to put everything back the way we want it. But, ideally, we could lock our warnings against change by other modules.
+We could write a second site-wide pragma to put everything back the way we want
+it. But, ideally, we could lock our warnings against change by other modules.
 
-    warnings::lock gives you this option.
+warnings::lock gives you this option.
 
 =head1 ADVANCED USAGE
 
-If you want to change the settings inside a particular code block, you can just issue a C<no warnings::lock> and then disable (or enable) those particular warnings.
+If you want to change the settings inside a particular code block, you can just
+issue a C<no warnings::lock> and then disable (or enable) those particular
+warnings.
 
     {
         no warnings::lock;
